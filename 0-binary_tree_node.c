83a4b87a34a55873a06036a2713c1913067f3ef5
FacundoV21 <6756@holbertonstudents.com>
2023-09-02 21:46:19 +0000

    fixed minor error when parent = NULL

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -4,22 +4,24 @@
  * binary_tree_node - creates a binary tree node
  * @parent: parent node of the node to create
  * @value: int value to put into the node
+ * Return: pointer to created node
 */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
-	if (!parent)
-		return(NULL);
-
 	new_node = malloc(sizeof(binary_tree_t));
 	if (!new_node)
-		return(NULL);
-	
-	new_node->parent = parent;
+		return (NULL);
+
+	if (!parent)
+		new_node->parent = NULL;
+	else
+		new_node->parent = parent;
+
 	new_node->n = value;
 	new_node->left = NULL;
 	new_node->right = NULL;
 
-	return(new_node);
+	return (new_node);
 }
